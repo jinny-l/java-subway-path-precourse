99f64ed62581977b59e4a46a745b578dcdfa15c8
jinny-l <jinny.l.dev@gmail.com>
2022-12-02 16:16:25 +0900

    feat: 경로 조회 기준을 선택하는 command 입력 값을 담당하는 객체 구현

---
+++ src/main/java/subway/constant/command/StandardCommand.java
@@ -0,0 +1,48 @@
+package subway.constant.command;
+
+public enum StandardCommand {
+
+    DISTANCE("최단 거리", "1"),
+    TIME("최소 시간", "2"),
+    BACK_TO_MAIN("돌아가기", "B");
+
+    private static final String FIELD_DELIMITER = ". ";
+
+    private final String text;
+    private final String command;
+
+    StandardCommand(String text, String command) {
+        this.text = text;
+        this.command = command;
+    }
+
+    private String getText() {
+        return text;
+    }
+
+    private String getCommand() {
+        return command;
+    }
+
+    public static String from(String input) {
+        validate(input);
+        return input;
+    }
+
+    private static void validate(String input) {
+        if (!input.equals(DISTANCE.getCommand())
+                && !input.equals(TIME.getCommand())
+                && !input.equals(BACK_TO_MAIN.getCommand())
+        ) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getCommand()
+                + FIELD_DELIMITER
+                + getText()
+                + "\n";
+    }
+}
